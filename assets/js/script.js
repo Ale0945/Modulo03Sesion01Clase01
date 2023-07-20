efa433bbc6d8cba22eda8baf2351a7e8f6176bfb
Alejandro Haro <aharo1005@e-camp.cl>
2023-07-20 12:56:10 -0400

    Trabajo arreglos js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-alert("Javascript")
+//arreglo simple
+/* var colaboradores = [
+    "Juan",
+    "Pedro",
+    "Francisco"
+] */
+
+var colaboradores = [
+    {
+        rut: "12.345.678-9",
+        nombre: "Juan Fica",
+        activo: true,
+        salario_hora:5150.65,
+        horas_trabajadas: 38
+    },
+    {
+        rut: "23.345.678-9",
+        nombre: "Andrea Pérez",
+        activo: true,
+        salario_hora:6150.85,
+        horas_trabajadas: 38
+    },
+    {
+        rut: "15.345.678-9",
+        nombre: "Francisco Gonzáles",
+        activo: false,
+        salario_hora:4850.85,
+        horas_trabajadas: 0
+    }
+]
+for (let index = 0; index < colaboradores.length; index++) {
+    var colaborador = colaboradores[index];
+    console.log(colaborador)
+}
+
